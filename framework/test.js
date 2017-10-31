9159a82ca3e8a638c56e94701523c9f668adceb0
xiongdeid1986 <xiongdeid1986@gmail.com>
2017-10-31 12:26:14 +0800

    up cached.

--- framework/test.js
+++
@@ -1,8 +0,0 @@
-var function_global = require("./namp/function_global.js")
-
-function_global.server_is_run("FileZilla Server",function(is){
-    console.log(is);
-})
-function_global.is_server("FileZilla Server",function(is){
-    console.log(is);
-})
